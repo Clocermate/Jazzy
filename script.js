6d0572ac9250b5ebc79b67e38c0a13a9e8cd07da
Clocermate <clocermate@gmail.com>
2023-06-26 21:43:01 +0530

    recreating it

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = () => {
-  if (window.scrollY > 100) {
-    document.querySelector('header').classList.add('active');
-  } else {
-    document.querySelector('header').classList.remove('active');
-  }
-};
